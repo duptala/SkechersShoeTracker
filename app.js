136ae4d11dfb53bdfef365d9d530e268a0f87915
Devesh Duptala <96435676+duptala@users.noreply.github.com>
2023-04-06 03:49:28 +1200

    Creating a dialog pop up when clicking on a grid

--- app.js
+++ app.js
@@ -1,9 +1,38 @@
 const searchButton = document.querySelector('.search-button');
 const searchBarInput = document.querySelector('.search-bar');
-const shoeCodeOutput = document.querySelector('.output');
+const shoeCodeOutputStatus = document.querySelector('.output-status');
 
-searchButton.addEventListener('click', () => { 
+searchButton.addEventListener("click", () => { 
     const code = searchBarInput.value;
 
-    shoeCodeOutput.textContent = code;
+    shoeCodeOutputStatus.textContent = code;
 })
+
+// adding all the necessary things
+const grids = document.querySelectorAll(".grid");
+const dialog = document.getElementById("dialog");
+const dialogContent = document.getElementById("dialog-content");
+const closeBtn = document.getElementsByClassName("close")[0];
+
+searchButton.addEventListener("click", () => {
+  const searchTerm = searchBar.value;
+  output.textContent = searchTerm;
+});
+
+grids.forEach((grid) => {
+  grid.addEventListener("click", () => {
+    const content = grid.dataset.content;
+    dialogContent.textContent = content;
+    dialog.style.display = "block";
+  });
+});
+
+closeBtn.addEventListener("click", () => {
+  dialog.style.display = "none";
+});
+
+window.addEventListener("click", (event) => {
+  if (event.target == dialog) {
+    dialog.style.display = "none";
+  }
+});
