ad5d9c3cbdd103b5562a8ef71b3f2a0e0b7212a4
Devesh Duptala <96435676+duptala@users.noreply.github.com>
2023-04-08 03:18:49 +1200

    REFACTOR + ADD: Added the highlighting functionality when a user types a corresponding grid data value

--- Skechers/app.js
+++ Skechers/app.js
@@ -1,8 +1,42 @@
 const searchButton = document.querySelector('.search-button');
-const shoeSearchInput = document.querySelector('.search-input');
-const queryResult = document.querySelector('.search-result')
+const searchBarInput = document.querySelector('.search-input');
+const queryResult = document.querySelector('.search-result');
+const grids = document.querySelectorAll(".grid-items");
 
-searchButton.addEventListener("click", () => {
-    const code = shoeSearchInput.value.trim();
-    queryResult.innerHTML = code;
-});
+// trying to find a dataset match when user enters shoe code
+searchButton.addEventListener("click", () => { 
+    const code = searchBarInput.value.trim();
+    queryResult.classList.remove("error");
+    queryResult.classList.remove("found");
+
+    if (code==="") { // if code empty, output error
+        queryResult.textContent = "⚠️ Please enter a valid shoe code! ⚠️"
+        queryResult.classList.add("error");
+        grids.forEach((grid) => {
+            grid.classList.remove("highlight");
+        });
+    } else {
+        let found = false;
+    grids.forEach((grid) => {
+      const content = grid.dataset.content;
+      if (content && code && content.split(",").map(c => c.trim()).includes(code)) {
+        grid.classList.add("highlight");
+        found = true;
+      } else {
+        grid.classList.remove("highlight");
+      }
+      if (found) {
+        queryResult.textContent = "Shoe found! 😄"
+        queryResult.classList.add("found");
+      } else {
+        queryResult.textContent = "Shoe NOT found.. 🙁";
+        queryResult.classList.remove("found");
+        queryResult.classList.add("error");
+      }
+    });
+    }
+  });
+
+  const dialog = document.getElementById("dialog");
+  const dialogContent = document.getElementById("dialog-content");
+
