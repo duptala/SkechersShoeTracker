6a15018a975c6c947443ae52de4deb039a03b5ff
Devesh Duptala <96435676+duptala@users.noreply.github.com>
2023-04-09 02:53:06 +1200

    REFACTOR: Refactored JS and added more dialog tweaks to make it easier for user input

--- Skechers/app.js
+++ Skechers/app.js
@@ -54,63 +54,14 @@ searchButton.addEventListener("click", () => {
     }
   });
 
-  // deleting all content
+  // deleting selected grids for when shoe boxes are moved
   deleteBtn.addEventListener("click", () => {
     grids.forEach((grid) => {
         grid.dataset.content = '';
       })
     });
 
-  // grids.forEach((grid) => {
-  //   grid.addEventListener("click", () => {
-  //     const gridId = grid.id;
-  //     const gridElement = document.getElementById(gridId);
-  
-  //     if (isEditMode) {
-  //       // Create a new instance of the edit dialog for this grid element
-  //       const editDialog = document.createElement("dialog");
-  //       editDialog.id = `edit-dialog-${gridId}`;
-  //       editDialog.innerHTML = `
-  //         <form>
-  //           <label for="grid-content-${gridId}">Content:</label>
-  //           <input type="text" id="grid-content-${gridId}" name="grid-content" value="${gridElement.getAttribute("data-content")}">
-  //           <button type="submit">Save</button>
-  //           <button type="button" id="cancel-btn">Cancel</button>
-  //         </form>
-  //       `;
-  
-  //       document.body.appendChild(editDialog);
-  
-  //       const contentInput = editDialog.querySelector(`#grid-content-${gridId}`);
-  //       const cancelButton = editDialog.querySelector("#cancel-btn");
-  
-  //       editDialog.showModal();
-  
-  //       cancelButton.addEventListener("click", () => {
-  //         editDialog.close();
-  //       });
-  
-  //       editDialog.addEventListener("close", () => {
-  //         // Remove the edit dialog from the DOM when it's closed
-  //         document.body.removeChild(editDialog);
-  //       });
-  
-  //       editDialog.querySelector("form").addEventListener("submit", (event) => {
-  //         event.preventDefault(); // prevents page refresh
-  //         const newContent = contentInput.value;
-  //         gridElement.setAttribute("data-content", newContent);
-  //         dialogContent.textContent = newContent;
-  //         editDialog.close();
-  //       });
-  //     } else {
-  //       const content = gridElement.getAttribute("data-content");
-  //       dialogContent.textContent = content;
-  //       dialog.style.display = "block";
-  //     }
-  //   });
-  // });
-
-  // preferred version:
+    // functionality for editing grid and updating values
   grids.forEach((grid) => {
     grid.addEventListener("click", () => {
       const gridId = grid.id;
@@ -122,6 +73,11 @@ searchButton.addEventListener("click", () => {
         const contentInput = document.createElement("input");
         const saveButton = document.createElement("button");
         const cancelButton = document.createElement("button");
+
+        editDialog.classList.add("edit-dialog");
+        contentInput.classList.add("edit-dialog-input");
+        saveButton.classList.add("edit-dialog-save-btn");
+        cancelButton.classList.add("edit-dialog-cancel-btn");
   
         editDialog.appendChild(contentInput);
         editDialog.appendChild(saveButton);
@@ -158,76 +114,7 @@ searchButton.addEventListener("click", () => {
       }
     });
   });
-
-
-  // testing version:
-  // grids.forEach((grid) => {
-  //   grid.addEventListener("click", () => {
-  //     const gridId = grid.id;
-  //     const gridElement = document.getElementById(gridId);
-  
-  //     if (isEditMode) {
-  //       // Create a new edit dialog for this grid element
-  //       const editDialog = document.createElement("dialog");
-  //       const contentInput = document.createElement("input");
-  //       const saveButton = document.createElement("button");
-  //       const cancelButton = document.createElement("button");
-  
-  //       editDialog.appendChild(contentInput);
-  //       editDialog.appendChild(saveButton);
-  //       editDialog.appendChild(cancelButton);
-  //       document.body.appendChild(editDialog);
-  
-  //       contentInput.type = "text";
-  //       contentInput.value = gridElement.getAttribute("data-content");
-  
-  //       saveButton.type = "button";
-  //       saveButton.textContent = "Save";
-  //       saveButton.addEventListener("click", () => {
-  //         const newContent = contentInput.value;
-  //         gridElement.setAttribute("data-content", newContent);
-  //         gridElement.textContent = newContent;
-  //         dialogContent.textContent = newContent;
-  //         editDialog.close();
-  //       });
-  
-  //       cancelButton.type = "button";
-  //       cancelButton.textContent = "Cancel";
-  //       cancelButton.addEventListener("click", () => {
-  //         editDialog.close();
-  //       });
-  
-  //       editDialog.showModal();
-  
-  //       editDialog.addEventListener("close", () => {
-  //         // Remove the edit dialog from the DOM when it's closed
-  //         document.body.removeChild(editDialog);
-  //       });
-  //     } else {
-  //       const content = gridElement.getAttribute("data-content");
-  //       dialogContent.textContent = content;
-  //       dialog.style.display = "block";
-  //     }
-  //   });
-  // });
-  
-    
-    
-    
-    
-    
-
-// grids.forEach((grid) => {
-//   grid.addEventListener("click", () => {
-//     const newContent = "New content"; // new content for the data-content attribute
-//     const gridID = grid.id;
-//     const gridElement = document.getElementById(gridID);
-//     gridElement.setAttribute("data-content", newContent); // set the new value of data-content
-//     console.log(gridElement.getAttribute("data-content")); // log the new value of data-content
-//   });
-// });
-
-
+   
 // close dialog
 closeBtn.addEventListener("click", () => {
   dialog.style.display = "none";
