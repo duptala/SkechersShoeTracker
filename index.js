04db478fcd95af01d477fcd684beff38b0fbc55a
Devesh Duptala <96435676+duptala@users.noreply.github.com>
2023-04-13 05:58:59 +1200

    CHANGE: Added environement variables for deployment in render

--- index.js
+++ index.js
@@ -2,48 +2,38 @@ const express = require('express');
 const app = express();
 const mysql = require('mysql');
 const cors = require('cors'); // for front-end interaction
+
 app.use(cors());
+app.use(express.json());
 
 app.listen(process.env.PORT || '3000', () => {
     console.log(`Server is running on port: ${process.env.PORT || '3000'}`);
 });
 
-// connection - ORIGINAL 
+// connection
 const db = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : null,
-    database : 'skechers_shoe_location'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
 });
 
-
 db.connect((err) => {
-    if(err){
+    if (err) {
         console.log("ERROR CONNECTING");
         throw err;
     }
     console.log("my sql connected!")
-})
-
-// GET SHOES IN SECTION - don't really need
-// app.get('/shoes/get/:shoeid', (req, res) => {
-//     const shoe_id = req.params.shoeid;
-//     let sql = `SELECT SECTION.section_name FROM SHOES JOIN SECTION ON SHOES.section_name = SECTION.section_name WHERE SHOES.shoeID LIKE '%${shoe_id}%';`;
-//     let query = db.query(sql, (err, result) => {
-//         if(err) throw err;
-//         res.json(result);
-//     })
-// })
+});
 
 // Get ALL shoes + their data
 app.get('/shoes/getallshoes', (req, res) => {
     let sql = 'SELECT * from shoes';
     let query = db.query(sql, (err, result) => {
-        if(err) throw err;
+        if (err) throw err;
         res.json(result);
     })
 })
-
 
 // UPDATE
 app.get('/shoes/update/:shoeid/:section_name', (req, res) => {
@@ -52,8 +42,7 @@ app.get('/shoes/update/:shoeid/:section_name', (req, res) => {
 
     let sql = `UPDATE SHOES SET SHOES.shoeID = '${shoe_id}' WHERE shoes.section_name = '${section_name}'`;
     let query = db.query(sql, (err, result) => {
-        if(err) throw err;
-        // res.json("result");
+        if (err) throw err;
         res.send("updated!");
     })
 })
@@ -65,26 +54,18 @@ app.get('/shoes/insert/:shoeid/:section_name', (req, res) => {
 
     let sql = `INSERT INTO SHOES (shoeid, section_name) VALUES ('${shoe_id}', '${section_name}')`;
     let query = db.query(sql, (err, result) => {
-        if(err) throw err;
-        // res.json("result");
+        if (err) throw err;
         res.send("shoes inserted/updated!");
     })
 })
 
 // Delete shoes based on section
 app.get('/shoes/delete/:section_name', (req, res) => {
-    // const section_name = gridElement.id;
     const section_name = req.params.section_name;
-  
+
     let sql = `DELETE FROM SHOES WHERE SHOES.section_name = '${section_name}'`;
     let query = db.query(sql, (error, result) => {
-      if (error) throw error;
-      res.send("shoes deleted!");
+        if (error) throw error;
+        res.send("shoes deleted!");
     });
-  });
-  
-
-
-
-
-
+});
