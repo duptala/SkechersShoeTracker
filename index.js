f25d2a38488517113303827fc29fbbb0be3be079
Devesh Duptala <96435676+duptala@users.noreply.github.com>
2023-04-14 03:29:44 +1200

    EDIT: Changed back the image directory path

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ app.get('/shoes/delete/:section_name', async (req, res) => {
 
   // serving front-end
 app.use(express.static(path.join(__dirname, "/Skechers"))); // for homepage 
-app.use(express.static(path.join(__dirname, "/images")));
+app.use(express.static(path.join(__dirname, "/images"))); // for image
 
 app.get("*", function (_, res) {
   res.sendFile(
